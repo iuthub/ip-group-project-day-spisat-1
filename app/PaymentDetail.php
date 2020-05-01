88d19bd5a1b5a0c14435aff82dbbdea59b3b44ab
Jakhangir Abdukadirov <jakhangir00@gmail.com>
2020-05-02 00:59:48 +0500

    Reservation hasOne PaymentDetail

--- app/PaymentDetail.php
+++ app/PaymentDetail.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentDetail extends Model
 {
-    //
+    public function room () {
+        return $this->belongsTo('App\Reservation');
+    }
 }
--- app/Reservation.php
+++ app/Reservation.php
@@ -9,4 +9,8 @@ class Reservation extends Model
     public function room () {
         return $this->belongsTo('App\Room');
     }
+
+    public function paymentDetail() {
+        return $this->hasOne('App\PaymentDetails');
+    }
 }
