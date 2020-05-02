75dcfe2352bc84869929b97c6dcbb95ee7557698
Alexandra Kim <alexandra.success.kim@gmail.com>
2020-05-02 16:35:01 +0500

    add rooms search

--- app/Room.php
+++ app/Room.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Room extends Model
 {
@@ -10,7 +11,29 @@ class Room extends Model
         return $this->belongsTo('App\RoomType');
     }
 
-    public function reservatios() {
+    public function reservations() {
         return $this->hasMany('App\Reservations');
+    }
+
+    public static function availableRooms($checkin, $checkout) {
+    	$roomsIds = DB::table('rooms')
+    		->leftjoin('reservations', function ($join) use ($checkin, $checkout) {
+            	$join->on('rooms.id', '=', 'reservations.room_id')
+                  ->where(function($query) use ($checkin, $checkout) {
+                    $query->where([
+                      ['checkin', '<=' , $checkin],
+                      ['checkout', '>=' , $checkout]
+                    ])->orWhere([
+                      ['checkin', '>=' , $checkin],
+                      ['checkin', '<=' , $checkin]
+                    ]);
+                  });
+            })
+			->where('reservations.id', null)
+			->select('rooms.*')
+			->get()
+			->toArray();
+			$rooms = Room::hydrate($roomsIds);
+        return $rooms;
     }
 }
--- resources/views/partials/left_menu.blade.php
+++ resources/views/partials/left_menu.blade.php
@@ -2,12 +2,13 @@
   <form method="post" action="{{ route('rooms') }}">
     @csrf
 
-    <p>From: <input type="date"></p>
-    <p>To: <input type="date"></p>
+    <p>From: <input type="date" name="checkin"></p>
+    <p>To: <input type="date" name="checkout"></p>
     <p>Number of people: 
-      <select>
+      <select name="number_of_guests">
         <option>1</option>
         <option>2</option>
+        <option>3</option>
         <option>4</option>
       </select>
     </p>
