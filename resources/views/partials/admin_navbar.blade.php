13ba2a8b4b845e3c731ac15c21a7dc95f208b170
Oybek Abdullaev <o.abdullaev2@student.inha.uz>
2020-05-08 20:52:22 +0500

    fix admin logout bug

--- resources/views/partials/admin_navbar.blade.php
+++ resources/views/partials/admin_navbar.blade.php
@@ -1,6 +1,16 @@
 <div style="border: 1px solid black; margin: 0 40px;">
   <ul style="display: inline-block">
     <li style="display: inline"><a href="{{ route('adminReservations') }}">Reservations</a></li>
-    <li style="display: inline"><a href="">Logout</a></li>
+    <li style="display: inline">
+      <a href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+          {{ __('Logout') }}
+      </a>
+    </li>
   </ul>
+
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+      @csrf
+  </form>
 </div>
