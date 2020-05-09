7d415cfce34398cbe8cea0f9d5b8127cbbc1a0ee
Jakhangir Abdukadirov <jakhangir00@gmail.com>
2020-05-09 19:35:23 +0500

    rooms in laravel

--- resources/views/hotel/rooms.blade.php
+++ resources/views/hotel/rooms.blade.php
@@ -1,27 +1,71 @@
-@extends('layouts.master')
+@extends('layouts.rmaster')
 
 @section('content')
-<div class="main" style="border: 1px solid black; overflow: hidden; margin: 40px;">
-  <h1>
+<div class="main">
+<div class="container-fluid">
+<div class="row">
+<div class="offset-md-1 col-md-5">
+  <h1 class="fstyle pageName">
     Rooms list
-  </h1>
+  </h1><br>
+  <?php
+  $i = 0;
+  ?>
   <ul>
   	@foreach ($rooms as $room)
-  	<li>
-  		<p>{{ $room->roomType->type }} - {{ $room->name }}</p>
-  		<p>{{ $room->main_picture_name }}</p>
-  		<p>{{ $room->roomType->price_per_night }}</p>
-  	</li>
-    <form method="post" action="{{ route('paymentDetails') }}">
-      @csrf
-      <input type="text" hidden value="{{ $checkin }}" name="checkin">
-      <input type="text" hidden value="{{ $checkout }}" name="checkout">
-      <input type="text" hidden value="{{ $number_of_guests }}" name="number_of_guests">
-      <input type="text" hidden value="{{ $room->id }}" name="room_id">
-      <input type="submit" value="order">
-    </form>
-  	@endforeach
-  </ul>
+    <?php
+  $container_class = ($i % 2) == 0 ? 'class1' : 'class2'
+    ?>
+    <div class="{{ $container_class }}">
+      <div class="container-fluid class1">
+       <div class="row">
+        <div class="offset-md-1 col-md-3">
+         <p class="photo">{{ $room->main_picture_name }}</p>
+        </div>
+        <div class="offset-md-1 col-md-5 fstyle1">
+         <ul>
+           <p class="fstyle">{{ $room->roomType->type }} - {{ $room->name }}</p><br>
+           <p class="mstyle">Price per night:{{ $room->roomType->price_per_night }}</p>
+            <form method="post" action="{{ route('paymentDetails') }}">
+            @csrf
+              <input type="text" hidden value="{{ $checkin }}" name="checkin">
+              <input type="text" hidden value="{{ $checkout }}" name="checkout">
+              <input type="text" hidden value="{{ $number_of_guests }}" name="number_of_guests">
+              <input type="text" hidden value="{{ $room->id }}" name="room_id">
+              <input type="submit" value="order">
+            </form>
+         </ul>
+        </div>
+       </div>
+      </div>
+    
+      <div class="container class2">
+       <div class="row">
+        <div class="offset-md-1 col-md-5">
+         <ul> 
+          <p class="fstyle">{{ $room->roomType->type }} - {{ $room->name }}</p><br>
+          <p class="mstyle">Price per night:{{ $room->roomType->price_per_night }}</p>
+           <form method="post" action="{{ route('paymentDetails') }}">
+           @csrf
+            <input type="text" hidden value="{{ $checkin }}" name="checkin">
+            <input type="text" hidden value="{{ $checkout }}" name="checkout">
+            <input type="text" hidden value="{{ $number_of_guests }}" name="number_of_guests">
+            <input type="text" hidden value="{{ $room->id }}" name="room_id">
+            <input type="submit" value="order">
+           </form>       
+        </ul>          
+        </div>
+        <div class="offset-md-2 col-md-4">
+          <p class="double-photo">{{ $room->main_picture_name }}</p>
+         </div>
+       </div>      
+      </div>
+ 
+   	@endforeach
+    </ul>
+   </div>
+  </div>
+  </div> 
 </div>
 
 @endsection
---
+++ resources/views/partials/rnavbar.blade.php
@@ -0,0 +1,58 @@
+<header class="hdr">
+    <nav class="container navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="#">
+        <img src="./images/Name.svg" alt="The Chemodan" />
+      </a>
+      <div class="collapse navbar-collapse">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item active reserve"><a class="nav-link" href="{{ route('reservations') }}">Reservations</a></li>
+    <li  class="nav-item active contact"><a class="nav-link" href="{{ route('contactUs') }}">Contact us</a></li>
+    @guest
+     
+        @if (Route::has('register'))
+         <li class="join">            
+            <form action="{{ route('register') }}">
+                <button type="submit" class="btn btn-success">{{ __('Join') }}</button>
+                </form>            
+            </li>
+        @endif
+          <li class="slash">
+            <img src="./images/Line 6.svg" alt="Line">
+          </li>
+
+          <li class="signin">
+            <form action="{{ route('login') }}">
+            <button type="submit" class="btn btn-success">{{ __('Sign In') }}</button>
+            </form>
+         </li>
+         <li class="icon">
+            <a class="nav-link disabled" tabindex="-1" aria-disabled="true">
+              <img src="./images/social-media 1.svg" height="30" alt="" />
+            </a>
+          </li>
+    @else
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </li>
+    @endguest
+  </ul>
+  </div>
+  </nav>
+  </header>
+
+
+  
