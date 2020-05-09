0c361c58267d19200fe544d583b5cd20435a32bb
Alexandra Kim <alexandra.success.kim@gmail.com>
2020-05-10 03:09:23 +0500

    fix urls

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,23 +3,23 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./css/style.css">
         <title>Hotel</title>
         <!-- Scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="./js/app.js" defer></script>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="shortcut icon" href="{{ asset('/img/travelling.png') }}">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/room.css') }}" rel="stylesheet">
+        <link rel="shortcut icon" href=".//img/travelling.png">
+        <link rel="stylesheet" href="./css/style.css">
+        <link href="./css/app.css" rel="stylesheet">
+        <link href="./css/room.css" rel="stylesheet">
     </head>
     <body>
       @include('partials.navbar')     
