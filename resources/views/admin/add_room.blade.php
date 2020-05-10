431e68ab688644ee81d18dd00d4831947c983b31
Alexandra Kim <alexandra.success.kim@gmail.com>
2020-05-10 21:34:23 +0500

    admin pages makeover

--- resources/views/admin/add_room.blade.php
+++ resources/views/admin/add_room.blade.php
@@ -2,39 +2,53 @@
 
 @section('content')
 <div class="col-md-9 mt-40">
-<div class="main ">
-<h1 class="titleName">
+<div class="main">
+  <h1 class="titleName">
     Add room
   </h1>
   <form method="post" action="{{ route('handleAddRoom') }}" enctype="multipart/form-data">
     @csrf
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="">Room name:</label> 
+        
+        <input class="form-control" type="text" name="name" placeholder="Name...">
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="">Room number:</label> 
+        <input class="form-control" type="text" name="room_number" placeholder="Number...">
+      </div>
+    </div>
+    
+    
+    <div class="form-row"> 
+      <div class="form-group col-md-6">
+        <label for="sel1">Main picture:</label> <br>
+        <input type="file"  name="picture_name" placeholder="Picture name...">
+      </div>
+    </div>
+    <div class="form-row"> 
+      <div class="form-group col-md-6">
+        <label for="sel1">Room type:</label> 
+        <select class="form-control" name="room_type_id">
+          <option disabled selected>
+            Select...
+          </option>
+          @foreach ($room_types as $room_type)
+            <option value="{{ $room_type->id }}">{{ $room_type->type }}</option>
+          @endforeach
+        </select>
+      </div>
+    </div>
     <p>
-      Room name: <input type="text" name="name" placeholder="Name...">
-    </p>
-    <p>
-      Room number: <input type="text" name="room_number" placeholder="Number...">
-    </p>
-    <p>
-      Main picture name: <input type="file" name="picture_name" placeholder="Picture name...">
-    </p>
-    <p>
-      Room type:
-      <select name="room_type_id">
-        <option disabled selected>
-          Select...
-        </option>
-        @foreach ($room_types as $room_type)
-          <option value="{{ $room_type->id }}">{{ $room_type->type }}</option>
-        @endforeach
-      </select>
-    </p>
-    <p>
-      <input type="submit" class="btn btn-success" value="Add">
+      <input class="btn btn-success" type="submit" value="Add">
     </p>
   </form>
 </div>
-</div></div>
- 
+</div>
 
- 
+
 @endsection
--- resources/views/admin/add_room_type.blade.php
+++ resources/views/admin/add_room_type.blade.php
@@ -1,38 +1,56 @@
 @extends('layouts.admin_layout')
 
 @section('content')
-
 <div class="col-md-9 mt-40">
 <div class="main">
-<h1 class="titleName">
-Add room
-</h1>
-<div class="container">
-<form method="post" action="{{ route('handleAddRoom') }}">
-@csrf
-<p>
-Room Type: <input type="text" name="type" placeholder="Type...">
-</p>
-<p>
-Price per Night: <input type="text" name="price_per_night" placeholder="Price...">
-</p>
-<p>
-Room Capacity: <input type="text" name="room_capacity" placeholder="Capacity...">
-</p>
-<p>
-Description: <textarea name="description" rows="5" cols="50"></textarea> 
+  <h1 class="titleName">
+    Add room type
+  </h1>
+  <form method="post" action="{{ route('handleAddRoomType') }}">
+    @csrf
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="">Type:</label> 
+        
+        <input class="form-control" type="text" name="type" placeholder="Type...">
+         </div>
+    </div>
 
-   <p>
-      <input type="submit" class="btn btn-success" value="Add">
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="">Price per night:</label> 
+        <input class="form-control"  type="text" name="price_per_night" placeholder="Price...">
+     </div>
+    </div>
+    
+    
+    <div class="form-row"> 
+      <div class="form-group col-md-6">
+        <label for="sel1">Room capacity:</label> 
+        <input class="form-control" type="text" name="room_capacity" placeholder="Capacity...">
+     </div>
+    </div>
+    
+    
+    <div class="form-row"> 
+      <div class="form-group col-md-6">
+        <label for="sel1">Area: </label> 
+        <input class="form-control"  type="text" name="area" placeholder="Area...">
+      </div>
+    </div>
+    
+    
+    <div class="form-row"> 
+      <div class="form-group col-md-6">
+        <label for="sel1">Description:</label> 
+        <textarea class="form-control" name="description" rows="5" cols="50"></textarea>
+     </div>
+    </div>
+    <p>
+      <input class="btn btn-success" type="submit" value="Add">
     </p>
-    </form>
+  </form>
 </div>
-
-  
-   
-</div>
-
-
 </div>
 
 
