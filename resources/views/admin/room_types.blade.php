d3b18e5ca06cae26dacf0cdc0ff36db4a72d0411
Alexandra Kim <alexandra.success.kim@gmail.com>
2020-05-10 16:21:43 +0500

    admin pages makeover

--- resources/views/admin/room_types.blade.php
+++ resources/views/admin/room_types.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 
-<div class="main">
-<div class="offset-md-2 col-md-9">
-  <h1 class="titleName">
-    Admin room types
-  </h1>
+<div class="col-md-9">
+  <div class="row mt-40">
+    <div class="col-10"><h1 class="titleName">Admin room types</h1></div>
+    <div class="col-2"><a class="btn btn-success" href="{{ route('adminAddRoomType') }}">Add room type</a></div>
+  </div>
   <table class="table">
 	  	<thead>
 		    <tr>
@@ -24,7 +24,7 @@
       <tbody>
       @foreach ($room_types as $room_type)
 
-        <tr class="table-primary">
+        <tr class="table">
           <th scope="row">{{ $room_type->id }}</th>
 		      <td>{{ $room_type->type }}</td>
 		      <td>{{ $room_type->price_per_night }}</td>
@@ -34,7 +34,7 @@
           <td>
             <form method="post" action="{{ route('editRoomType') }}">
               @csrf
-              <input type="submit" value="Edit">
+              <input class="btn btn-outline-info" type="submit" value="Edit">
               <input type="hidden" name="room_type_id" value="{{ $room_type->id }}">
             </form>
           </td>
@@ -42,7 +42,7 @@
             <form method="post" action="{{ route('deleteRoomType') }}">
               @csrf
               <input type="hidden" name="room_type_id" value="{{ $room_type->id }}">
-              <input type="submit" value="Delete">
+              <input class="btn btn-outline-secondary" type="submit" value="Delete">
             </form>
           </td>
         </tr>
@@ -50,9 +50,8 @@
       @endforeach
       </tbody>
     </table>
-  <a href="{{ route('adminAddRoomType') }}"><button>Add room type</button></a>
-  </div>
-</div>
+   </div>
+
 
 
 
